01c2575967c4e0d7d0a215016dd747ef4630789a
AbduvaliMamatov <thebestlittleprince@gmail.com>
2025-08-14 00:03:33 +0500

    Fix typo in log message for unhandled exceptions in CustomExceptionHandlerMiddleware

--- Stackup.Quiz.Api/Middlewares/CustomExceptionHandlerMiddleware.cs
+++ Stackup.Quiz.Api/Middlewares/CustomExceptionHandlerMiddleware.cs
@@ -15,7 +15,7 @@ public class CustomExceptionHandlerMiddleware(
         }
         catch (Exception ex)
         {
-            logger.LogError(ex, "Unhandled exception occured");
+            logger.LogError(ex, "Unhandled exception occurred");
 
             (int code, string message) = ex switch
             {
